27fc9c4d7756b6e320aec5632653a496f833ec95
Alvar <valenciawalas@hotmail.com>
2021-01-16 15:33:39 +0100

    :

--- mediador/autenticado.js
+++ mediador/autenticado.js
@@ -6,31 +6,38 @@ var moment = require('moment');
 var secret = 'NLB3JWl@U+5zgdyE5';
 
 //esta funcion comprobara si los datos del token son correctos, se ejecutara antes de la accion del controlador
-exports.ensureAuth = function(req, res, next){
+exports.asegurarAutenticacion = function(req, res, next){
 
 	//TODO y si no quiero que todos los usuarios esten logueados?? el flujo no pasaria del primer IF
 
-	//si no nos llega el token por authorization
-	if(!req.headers.authorization){
+	//si no nos llega el token por la cabecera, autorizacion
+	if(!req.headers.autorizacion){
 		return res.status(403).send({message: 'La petición no tiene la cabecera de autenticación'});
 	}
 
 
-	//guardamos el token
-	var token = req.headers.authorization.replace(/['"]+/g, '');
+	//si llegamos aqui tenemos la cabecera autorizacion
+	//guardamos el token sin comillas
+	var token = req.headers.autorizacion.replace(/['"]+/g, '');
 
+	//decodificacion
 	try{
+		//decodificamos y guardamos el token
 		var payload = jwt.decode(token, secret);
 
+		//comprobamos si ha expirado el token
 		if(payload.exp <= moment().unix()){
 			return res.status(401).send({message: 'El token ha expirado'});
 		}
+	//capturamos error si hubiera fallo
 	}catch(ex){
-		//console.log(ex);
+		console.log(ex);
 		return res.status(404).send({message: 'Token no válido'});
 	}
 
-	req.user = payload;
+	//añadimos a la request una propiedad con un objeto con todos los datos del usuario
+	req.usuarioDesencriptado = payload;
 
+	//salimos de este mediador
 	next();
 };
--- rutas/usuarioRuta.js
+++ rutas/usuarioRuta.js
@@ -4,12 +4,17 @@
 var express = require('express');
 //cargamos el UsuarioController
 var UsuarioController = require('../controlador/usuarioControlador');
-
 //cargamos el router de expres
 var api = express.Router();
+//cargamos el mediador de autenticacion
+var mediador_autoriza = require('../mediador/autenticado');
+
+
+//TODO y si no quiero que todos los visitantes esten logueados?? distintas rutas para logueados o no logueados?
 
 //cargamos rutas
-api.get('/probando-controlador', UsuarioController.pruebas);
+api.get('/probando-controlador', mediador_autoriza.asegurarAutenticacion, UsuarioController.pruebas); 
+//sin pasar por autenticacion //api.get('/probando-controlador', UsuarioController.pruebas);
 api.post('/registrar', UsuarioController.guardarUsuario);
 api.post('/login', UsuarioController.loginUsuario);
 
